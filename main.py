3c0e38a17e227c981742e7170269bf14744bd133
vanessa-ig <66692723+vanessa-ig@users.noreply.github.com>
2022-06-07 19:50:05 +0100

    Accessed segmentation folder of a specific video

---
+++ main.py
@@ -0,0 +1,6 @@
+import windowFrames
+
+
+# dirname for now is the directory of the video e.g. 'Video01', but will change in the future to CaDISv2 and loop through this 
+dirname = '/Users/vanessaigodifo/Library/CloudStorage/OneDrive-UniversityCollegeLondon/MSc_Project/Dataset/CaDISv2/Video01'
+windowFrames.getWindow(dirname)
--- windowFrames.py
+++ windowFrames.py
@@ -3,25 +3,25 @@ import argparse
 import os 
 import cv2
 
-def extractImages(pathIn, pathOut):
-    count = 0
-    vidcap = cv2.VideoCapture(pathIn)
-    success,image = vidcap.read()
-    success = True
-    while success:
-        vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000))    # added this line 
-        success,image = vidcap.read()
-        print ('Read a new frame: ', success)
-        cv2.imwrite( pathOut + "\\frame%d.jpg" % count, image)     # save frame as JPEG file
-        count = count + 1
+# def extractImages(pathIn, pathOut):
+#     count = 0
+#     vidcap = cv2.VideoCapture(pathIn)
+#     success,image = vidcap.read()
+#     success = True
+#     while success:
+#         vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000))    # added this line 
+#         success,image = vidcap.read()
+#         print ('Read a new frame: ', success)
+#         cv2.imwrite( pathOut + "\\frame%d.jpg" % count, image)     # save frame as JPEG file
+#         count = count + 1
 
-if __name__=="__main__":
-    a = argparse.ArgumentParser()
-    a.add_argument("train01.mp4", help="path to video")
-    a.add_argument("TESTtrain01.mp4", help="path to images")
-    args = a.parse_args()
-    print(args)
-    extractImages(args.pathIn, args.pathOut)
+# if __name__=="__main__":
+#     a = argparse.ArgumentParser()
+#     a.add_argument("train01.mp4", help="path to video")
+#     a.add_argument("TESTtrain01.mp4", help="path to images")
+#     args = a.parse_args()
+#     print(args)
+#     extractImages(args.pathIn, args.pathOut)
 
 
 
@@ -34,8 +34,37 @@ def getWindow(dirname):
 
     # find the video number to be processed 
     if os.path.exists(dirname):
-      print('yes')
+
+      # get the video number
+      vid_num = dirname[-2:]
+      print(vid_num)
+
+      # find the corresponding segmentation
+      video_segmentation_folder = dirname + '/Labels'
+      os.chdir(dirname + '/Labels')
+
+      # loop through all the segmentations in this directory
+      for filename in os.listdir(dirname):
+
+        # ignoring hidden files
+        if not filename.startswith('.'):
+            segmentation = os.path.join(dirname, filename)
+
+            # checking if it is a file
+            if os.path.isfile(segmentation):
+                
+                # find the frame number 
+                frame_num = segmentation[-3:]
+
+                # find the corresponding video in the workflow folder 
+                wf_path = '/Volumes/WD Drive/MSc Project/CATARACTS_WF_2020/Videos/train' + vid_num
+
+                os.chdir(wf_path)
+                print(os.getcwd())
+
+
 
 
     # find the frame number of the segmentation in the video 
     return 
+
