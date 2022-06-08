2006b9f35753302ec19cbbf63109dc8eca239280
vanessa-ig <66692723+vanessa-ig@users.noreply.github.com>
2022-06-08 12:47:18 +0100

    Frame rate of WF video 1 found to be 29.183

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import windowFrames
 
+# specify video to create optical flows for 
+video = 'Video01'
 
 # dirname for now is the directory of the video e.g. 'Video01', but will change in the future to CaDISv2 and loop through this 
-dirname = '/Users/vanessaigodifo/Library/CloudStorage/OneDrive-UniversityCollegeLondon/MSc_Project/Dataset/CaDISv2/Video01'
-windowFrames.getWindow(dirname)
+dirname = '/Users/vanessaigodifo/Library/CloudStorage/OneDrive-UniversityCollegeLondon/MSc_Project/Dataset/CaDISv2/' + video 
+windowFrames.generateFlows(dirname)
--- windowFrames.py
+++ windowFrames.py
@@ -3,46 +3,68 @@ import argparse
 import os 
 import cv2
 
-# def extractImages(pathIn, pathOut):
-#     count = 0
-#     vidcap = cv2.VideoCapture(pathIn)
-#     success,image = vidcap.read()
-#     success = True
-#     while success:
-#         vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000))    # added this line 
-#         success,image = vidcap.read()
-#         print ('Read a new frame: ', success)
-#         cv2.imwrite( pathOut + "\\frame%d.jpg" % count, image)     # save frame as JPEG file
-#         count = count + 1
+def extractImages(pathIn, pathOut):
+    count = 0
+    vidcap = cv2.VideoCapture(pathIn)
+    success,image = vidcap.read()
+    success = True
+    while success:
+        vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000))    # added this line 
+        success,image = vidcap.read()
+        print ('Read a new frame: ', success)
+        cv2.imwrite( pathOut + "\\frame%d.jpg" % count, image)     # save frame as JPEG file
+        count = count + 1
 
-# if __name__=="__main__":
-#     a = argparse.ArgumentParser()
-#     a.add_argument("train01.mp4", help="path to video")
-#     a.add_argument("TESTtrain01.mp4", help="path to images")
-#     args = a.parse_args()
-#     print(args)
-#     extractImages(args.pathIn, args.pathOut)
-
-
+if __name__=="__main__":
+    a = argparse.ArgumentParser()
+    a.add_argument("train01.mp4", help="path to video")
+    a.add_argument("TESTtrain01.mp4", help="path to images")
+    args = a.parse_args()
+    print(args)
+    extractImages(args.pathIn, args.pathOut)
 
 
+
+def getWindow(wf_file, save_path, frame_num):
+
+  count = 0 
+
+  # open the workflow video file
+  vidcap = cv2.VideoCapture(wf_file)
+
+  # access the image array 
+  success, image = vidcap.read()
+  # success = True
+
+  # if video successfully read
+  while success:
+    # fps is 30 fps
+    print(vidcap.get(cv2.CAP_PROP_FPS))
+
+
+      # vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000))    # added this line 
+      # success,image = vidcap.read()
+      # print ('Read a new frame: ', success)
+      # cv2.imwrite(save_path + "\\frame%d.jpg" % count, image)     # save frame as JPEG file
+      # count = count + 1
+  
+
+  return 
 
 
 
 ''' Function to get a window of 50 frames before and after the frame of '''
-def getWindow(dirname):
+def generateFlows(dirname):
 
   # find the video number to be processed 
   if os.path.exists(dirname):
 
     # get the video number
     vid_num = dirname[-2:]
-    print(vid_num)
 
     # find the corresponding segmentation
     video_segmentation_folder = dirname + '/Labels'
     os.chdir(dirname + '/Labels')
-    print(os.getcwd())
 
     # loop through all the segmentations in this directory
     for filename in os.listdir(video_segmentation_folder):
@@ -60,6 +82,10 @@ def getWindow(dirname):
               # find the corresponding video in the workflow folder 
               wf_file = '/Volumes/WD_Drive/MSc_Project/CATARACTS_WF_2020/Videos/train' + vid_num + '.mp4'
               
-              getWindow(wf_file, frame_num)
+              # define save path for window centered on segmentation 
+              save_path = '/Volumes/WD_Drive/MSc_Project/Windows/Video' + vid_num + '/'
+
+              # generate video for the window of frames centered on segmentation
+              getWindow(wf_file, save_path, frame_num)
   return 
 
